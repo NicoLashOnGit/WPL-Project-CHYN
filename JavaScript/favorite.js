ef30b2c0297300ff123b6077fe9fa49c447024c0
Fullpower79 <Cedricadriaenssens11@gmail.com>
2025-03-15 09:28:05 +0000

    Nieuwe branch voor de favorite button

--- JavaScript/favorite.js
+++ JavaScript/favorite.js
@@ -1,14 +1,3 @@
-const star = document.getElementsByClassName("characterPageBtnImages");
-const button = document.getElementById('blacklistBtn');
-
-let isFirstImage = true;
-
-button.addEventListener("click", function() {
-    console.log('test')
-    if (isFirstImage) {
-        star.src = "/Images/ButtonImages/FilledGoldFavouriteStar.png";
-    } else {
-        star.src = "/Images/ButtonImages/blackEmptyFavouriteStar.png"
-    }
-    isFirstImage = !isFirstImage
-});
+function myFunction () {
+    document.getElementById("blacklistBtn")
+}
