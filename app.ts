b04ff91b02c7d0ee79266c8917a5933edc5ae007
Fullpower79 <Cedricadriaenssens11@gmail.com>
2025-04-06 09:25:14 +0000

    alle app.gets toegevoegd. command node app.ts werkt nog altijd niet

--- app.ts
+++ app.ts
@@ -11,3 +11,38 @@ app.listen(app.get("port"), async() => {
     console.log("[server] http://localhost:" + app.get("port"))
 });
 
+app.get("/", (req, res) => {
+    res.render("Landingpage")
+});
+
+app.get("/Accountpage", (req,res) => {
+    res.render("Accountpage")
+})
+
+app.get("/blacklist", (req, res) => {
+    res.render("blacklist")
+})
+
+app.get("/CharacterInfo", (req,res) => {
+    res.render("CharacterInfo")
+})
+
+app.get("/Faq", (req,res) => {
+    res.render("Faq")
+})
+
+app.get("/favoritePage", (req,res) => {
+    res.render("favoritePage")
+})
+
+app.get("/Homepage", (req,res) => {
+    res.render("Homepage")
+})
+
+app.get("/Landingpage", (req, res) => {
+    res.render("Landingpage")
+})
+
+app.get("/Loginpage", (req, res) => {
+    res.render("Loginpage")
+})
