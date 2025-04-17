fe5e1a354db4ff67c74028e5807209dbd5a5d4c2
Fullpower79 <Cedricadriaenssens11@gmail.com>
2025-04-17 13:14:20 +0000

    paginas switchen werkt. De effectieve code gaat moeten wachten tot de database klaargezet is

--- app.ts
+++ app.ts
@@ -7,42 +7,46 @@ app.use(express.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.set("port", 3000);
 
+
 app.listen(app.get("port"), async() => {
     console.log("[server] http://localhost:" + app.get("port"))
 });
 
 app.get("/", (req, res) => {
-    res.render("Landingpage")
+    res.render("Landingpage", {title: "Landingpage"})
 });
 
 app.get("/Accountpage", (req,res) => {
-    res.render("Accountpage")
+    res.render("Accountpage", {title: "Account"})
 })
 
 app.get("/blacklist", (req, res) => {
-    res.render("blacklist")
+    res.render("blacklist", {title: "Blacklist"})
 })
 
 app.get("/CharacterInfo", (req,res) => {
-    res.render("CharacterInfo")
+    res.render("CharacterInfo", {title: "Character Info"})
 })
 
 app.get("/Faq", (req,res) => {
-    res.render("Faq")
+    res.render("Faq", {title: "FAQ"})
 })
 
 app.get("/favoritePage", (req,res) => {
-    res.render("favoritePage")
+    res.render("favoritePage", {title: "Favorite"})
 })
 
 app.get("/Homepage", (req,res) => {
-    res.render("Homepage")
-})
-
-app.get("/Landingpage", (req, res) => {
-    res.render("Landingpage")
+    res.render("Homepage", {title: "Home"})
 })
 
 app.get("/Loginpage", (req, res) => {
-    res.render("Loginpage")
+    res.render("Loginpage", {title: "Login"})
 })
+
+app.get("/Characterpage", (req, res) => {
+    res.render("Characterpage", {title: "Character"})
+})
+app.get("/Landingpage", (req, res) => {
+    res.render("Landingpage", {title: "Landingpage"})
+});
---
+++ public/TypeScript/winLoseButton.ts
@@ -0,0 +1,62 @@
+function getElement<T extends HTMLElement>(id: string): T {
+    const el = document.getElementById(id);
+    if (!el) throw new Error(`Element with ID "${id}" not found`);
+    return el as T;
+}
+
+const winCountElement = getElement<HTMLSpanElement>('win-count');
+const loseCountElement = getElement<HTMLSpanElement>('lose-count');
+const winIncreaseButton = getElement<HTMLButtonElement>('IncreaseWinBtn1');
+const loseIncreaseButton = getElement<HTMLButtonElement>('IncreaseLossnBtn1');
+const winDecreaseButton = getElement<HTMLButtonElement>('DecreaseWinBtn1');
+const loseDecreaseButton = getElement<HTMLButtonElement>('DecreaseLossBtn1');
+const gameCount = getElement<HTMLSpanElement>('gameCount');
+const WinRate = getElement<HTMLSpanElement>('WinRate');
+
+let winCount = 0;
+let loseCount = 0;
+
+function updateStats(): void {
+    const totalGames = winCount + loseCount;
+    const winPercent = totalGames > 0 ? winCount / totalGames : 0;
+
+    winCountElement.textContent = `\xA0 ${winCount}`
+    loseCountElement.textContent = `\xA0 ${loseCount}`
+    gameCount.textContent = `\xA0 ${totalGames}`
+    WinRate.textContent = `\xA0 ${(winPercent*100).toFixed(2)}`
+}
+
+winIncreaseButton.addEventListener("click", () => {
+    winCount++;
+    updateStats();
+});
+
+winDecreaseButton.addEventListener('click', () => {
+    if (winCount == 0) {
+        winCount = 0;
+    }
+    else {
+        winCount--;
+    };
+    updateStats();
+});
+
+loseIncreaseButton.addEventListener('click', () => {
+    if (loseCount == (winCount * 3)) {
+        const parentArticle = loseCountElement.closest('article');
+        if (parentArticle) {
+            parentArticle.style.display = 'none'
+        }
+    }
+    updateStats();
+});
+
+loseDecreaseButton.addEventListener('click', () => {
+    if (loseCount == 0) {
+        loseCount = 0;
+    }
+    else {
+        loseCount--;
+    }
+    updateStats();
+})
